bf0bc88e3039cec5cf8a4d357e4f1cebcc7aaf01
oagholor <oagholor@umich.edu>
2026-08-06 22:15:41 -0400

    Oregan 2021 PE Degradation with improved data matching Ghosh calc

--- pybamm/mz_develop/test_pe_degradation_SPM_umich.py
+++ pybamm/mz_develop/test_pe_degradation_SPM_umich.py
@@ -8,7 +8,7 @@ import pybamm.mz_develop.output_module as outmod
 #%%
 model = pybamm.lithium_ion.SPM({"PE degradation": "yes"})
 #%
-param = pybamm.ParameterValues(chemistry=pybamm.parameter_sets.ORegan2021_pe_deg)
+param = pybamm.ParameterValues(chemistry=pybamm.parameter_sets.ORegan2021_pe_deg_v2)
 #%%
 experiment = pybamm.Experiment(
     [
@@ -28,46 +28,20 @@ experiment = pybamm.Experiment(
 sim = pybamm.Simulation(
     model, experiment=experiment,
     parameter_values=param,
+    solver=pybamm.CasadiSolver(mode="safe", dt_max=600),
 )
 
 
 #%%
-solution = sim.solve(calc_esoh=False)
+try:
+    solution = sim.solve(calc_esoh=False)
+except pybamm.SolverError as e:
+    print("Solve stopped early:", e)
+    solution = sim.solution
 
 
     #%%
 output_variables = outmod.output_variables_spm
 sim.plot(output_variables)
 
-#%%
-total_cycles = len(solution.cycles)
-Q_dis_cycles = []
-cycle_numbers = np.arange(1, total_cycles+1)
-for i in range(total_cycles):
-    Q_dis_cyc = solution.cycles[i].steps[3]["Discharge capacity [A.h]"].entries
-    Q_dis = Q_dis_cyc[-1] - Q_dis_cyc[0]
-    Q_dis_cycles.append(Q_dis)
-
-#%%
-f = open("pybamm\mz_develop\output\capacity.txt", "a")
-np.savetxt(f, np.c_[cycle_numbers, Q_dis_cycles], fmt='%12.6f', delimiter=', ')
-f.close()
-#%%
-plt.figure(figsize=(8, 6))
-# markerfacecolor
-plt.plot(cycle_numbers, Q_dis_cycles, 'o', mfc='none', label="LAM and LLI")
-plt.legend()
-
-#%%
-# np.save('Q_dis_cycles_2', Q_dis_cycles_2)
-Q_dis_cycles_0 = np.load('Q_dis_cycles_0.npy')
-Q_dis_cycles_1 = np.load('Q_dis_cycles_1.npy')
-
-#%%
-plt.figure(figsize=(8, 6))
-# markerfacecolor
-plt.plot(cycle_numbers, Q_dis_cycles_0, '-o', mfc='none', label="only LAM")
-plt.plot(cycle_numbers, Q_dis_cycles_1, '-s', mfc='none', label="LAM and LLI")
-plt.plot(cycle_numbers, Q_dis_cycles_2, '-*', mfc='none', label="LAM, LLI, and shell")
-plt.legend()
 
